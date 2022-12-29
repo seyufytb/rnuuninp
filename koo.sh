c04d6ea8f6b529b34b88d6bfd175e7eb7c819fb3
seyufytb <121601008+seyufytb@users.noreply.github.com>
2022-12-29 15:00:45 +0800

    Add files via upload

---
+++ koo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+cat <<EOF >/config.json
+{
+    "log": {
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "listen": "0.0.0.0",
+            "port": 443,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "6cb4a758-45ca-4368-9a62-b1d0cf8d89ab",
+                        "alterId": 0
+                    }
+                ],
+                "disableInsecureEncryption": false
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+/usr/local/bin/ttcc -config=/config.json
